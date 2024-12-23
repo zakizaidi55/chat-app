ceedb5b1ff232f08e5c67a3cc5c864293adc6a34
zakizaidi55 <zakizaidi55@gmail.com>
2024-12-23 10:55:58 +0530

    add room logic

--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,39 @@ import {WebSocketServer, WebSocket} from "ws";
 
 
 const wss = new WebSocketServer({port:8080});
-
+interface User {
+    socket:WebSocket,
+    room:string
+}
 let userCount = 0;
-let allSockets:WebSocket[] = [];
-wss.on("connection", (socket) => {
-    allSockets.push(socket);
-    console.log("User connected");
-    userCount++;    
-
+// let allSockets:WebSocket[] = [];
+let allSocket:User[] = [];
+wss.on("connection", (socket) => {    
     socket.on("message", (message) => {
-        console.log("message is received "+ message.toString());
+        const parsedMessage = JSON.parse(message as unknown as string);
+            if(parsedMessage.type === "join") {
+                allSocket.push( {
+                    socket,
+                    room: parsedMessage.payload.roomId
+                })
+            }
 
-        for(let i=0; i<allSockets.length; i++) {
-            const s = allSockets[i];
-            s.send(message.toString () + ": sent from the server ");
-            
-        }
-
-    }) 
+            if(parsedMessage.type === "chat") {
+                //first I need to find the room id of current user, which room id it joined
+                // const currentUserRoom = allSocket.find((x) => x.socket === socket).room;
+                let currentUserRoom = null;
+                
+                for(let i=0; i<allSocket.length; i++) {
+                    if(allSocket[i].socket === socket)
+                        currentUserRoom = allSocket[i].room;
+                }
+                
+                //send to the message to all the room members
+                for(let i=0; i<allSocket.length; i++) {
+                    if(allSocket[i].room == currentUserRoom)
+                        allSocket[i].socket.send(parsedMessage.payload.message);
+                }
+            }
+    })  
+    
 })
